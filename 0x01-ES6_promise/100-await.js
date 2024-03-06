2df2a0851275edb0a3b7385743714e8c73b15673
fatima-elfat <ffzelfatehy@gmail.com>
2024-03-06 19:53:19 +0000

    add files of advanced tasks

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,16 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  let results = {};
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    results = { photo, user };
+  } catch (error) {
+    results = {
+      photo: null,
+      user: null,
+    };
+  }
+  return results;
+}
