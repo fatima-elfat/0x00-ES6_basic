e12e3402ab8e65ba84f2803eb62640fa1a7189dd
fatima-elfat <ffzelfatehy@gmail.com>
2024-03-12 20:24:41 +0000

    task 8: fix errors

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -9,12 +9,14 @@ export default function cleanSet(set, startString) {
   * return [...set].filter((val) => val.startsWith(startString))
   *  .map((val) => val.slice(startString.length)).join('-');
   */
-  const vals;
+  const vals = [];
   for (const val of set.values()) {
     if (val.startsWith(startString)) {
-      const r = value.substring(startString.length);
+      const r = val.substring(startString.length);
       if (r !== val) {
         vals.push(r);
-      }}}
+      }
+    }
+  }
   return vals.join('-');
 }
