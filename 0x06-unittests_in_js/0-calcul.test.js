2ee7cdcae46cbd46080ec0ff8414adc40d8d5cff
fatima-elfat <ffzelfatehy@gmail.com>
2024-05-02 19:58:09 +0000

    task 0: add 4 tests

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,8 +2,20 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
-    it('shoud return 4 as int', () => {
-      assert.strictEqual(calculateNumber(2.0, 2.0), 4);
+    it('shoud return 3', () => {
+      assert.strictEqual(calculateNumber(2.0, 1.0), 3);
+    });
+    it('should return 4', function () {
+      assert.strictEqual(calculateNumber(2.8, 1), 4);
+    });
+    it('should return 5', function () {
+      assert.strictEqual(calculateNumber(3.39, 3.39), 6);
+    });
+    it('should return 7', function () {
+      assert.strictEqual(calculateNumber(3.0, 3.9), 7);
+    });
+    it('should return 8', function () {
+      assert.strictEqual(calculateNumber(4.9, 3.0), 8);
     });
 });
 
